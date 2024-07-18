e4a353cf8873ffe698f9c2c2562e6c5c06d28092
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-07-17 19:24:49 -0500

    .

---
+++ AdityaShroff/detail.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>RAMT coffee_mug_insulated</title>
+  <link rel="stylesheet" type="text/css" href="../HelperFiles/style.css">
+
+</head>
+<body>
+  <?php
+    require_once("../HelperFiles/header.php");
+  ?>
+  
+
+  <main>
+
+<!-- Write code here -->
+
+
+</main>
+
+
+<?php
+    require_once("../HelperFiles/footer.php");
+  ?>
+</body>
+</html>
