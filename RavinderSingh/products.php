fbc95c317afec61e94a83eb37965c6054a7101ff
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-07-19 16:21:10 -0500

    .

---
+++ RavinderSingh/products.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>RAMT Coffee</title>
+  <link rel="stylesheet" type="text/css" href="../HelperFiles/style.css">
+
+</head>
+<body>
+  <?php
+    require_once("../HelperFiles/header.php");
+  ?>
+  
+
+  <main>
+
+<!-- Write code here -->
+
+
+</main>
+
+
+<?php
+    require_once("../HelperFiles/footer.php");
+  ?>
+</body>
+</html>
